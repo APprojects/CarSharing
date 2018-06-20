6e5fd20d045142cc6e26e3dfa278fe99fb5fd69a
Matteo Azzarelli <matteo.azzarelli@studenti.unipg.it>
2018-06-20 18:02:28 +0200

    update matteo

---
+++ www/test.php
@@ -0,0 +1,28 @@
+<?php
+// Data in JSON format
+$data = array(
+    'id' => '1',
+);
+ 
+$payload = json_encode($data);
+ 
+// Prepare new cURL resource
+$ch = curl_init('http://localhost:8080/php_rest_myblog/api/user/read_single.php');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLINFO_HEADER_OUT, true);
+curl_setopt($ch, CURLOPT_POST, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
+ 
+// Set HTTP Header for POST request 
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'Content-Type: application/json',
+    'Content-Length: ' . strlen($payload))
+);
+ 
+// Submit the POST request
+$result = curl_exec($ch);
+ 
+// Close cURL session handle
+curl_close($ch);
+
+?>
