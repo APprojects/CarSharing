dc4d1213865217fe28b7e8e3f52ba498c8fc3713
Matteo Azzarelli <matteo.azzarelli@studenti.unipg.it>
2018-06-23 14:17:29 +0200

    update

--- www/welcomeCustomer.php
+++ www/welcomeCustomer.php
@@ -73,7 +73,7 @@
 										<div class="tab-content">
 											<div class="tab-content-inner active" data-content="signup">
 												<h3>Book Your Electric Car!</h3>
-												<form action="order.php">
+												<form action="order.php" method="post">
 													
 													<div class="row form-group">
 														<div class="col-md-12">
