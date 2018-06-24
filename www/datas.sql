8795a830142b6e6c0d1ceccefd8bb44927b397c8
Matteo Azzarelli <matteus1991@gmail.com>
2018-06-24 20:34:34 +0200

    Edit database

--- www/datas.sql
+++ www/datas.sql
@@ -24,6 +24,7 @@ CREATE TABLE IF NOT EXISTS Car (
 );
 
 CREATE TABLE IF NOT EXISTS Basement (
+	id integer PRIMARY KEY AUTO_INCREMENT, 
 	name  varchar(50) PRIMARY KEY,
 	address varchar(50),
 	seller integer REFERENCES User(id)
