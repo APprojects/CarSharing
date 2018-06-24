94e0a1002100e039c3a2a47ae176d44453b4cb47
Matteo Azzarelli <matteo.azzarelli@studenti.unipg.it>
2018-06-24 12:34:18 +0200

    update logout
    after 5 seconds it go back to the hompage

--- www/logout.php
+++ www/logout.php
@@ -1,6 +1,8 @@
 <?php
 	session_start();
 	session_destroy();
-	echo "you succesfully logout. click here to login again <a href='login.php'>login again</a>";
+	header( "refresh:5;url=index.php" );
+	print_r( "you succesfully logout. Wait 5 seconds or click here to go back to the <a href='login.php'>Home Page</a>");
+	
 
 ?>
