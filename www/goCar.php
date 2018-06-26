74a4ade9fca89626318a05df2d871576dc3f7154
PiersantiLuisa <luisa.piersanti@studenti.unipg.it>
2018-06-26 12:08:31 +0200

    update baement and car

--- www/goCar.php
+++ www/goCar.php
@@ -40,9 +40,9 @@
 	<link rel="stylesheet" href="css/owl.theme.default.min.css">
 
 	<!-- Theme style  -->
-	<link rel="stylesheet" href="css/style1.css">
+	
 	<link rel="stylesheet" href="css/style.css">
-
+<link rel="stylesheet" href="css/style1.css">
 	<!-- Modernizr JS -->
 	<script src="js/modernizr-2.6.2.min.js"></script>
 	<!-- FOR IE9 below -->
